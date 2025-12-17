8c384b9ea93d4b2ad5d5961acc73d4de38a060b9
Edward Gilbert <egbot@asu.edu>
2025-12-17 12:27:37 -0500

    Add vol 21

--- menu_right.php
+++ menu_right.php
@@ -23,6 +23,7 @@
 		<p><a href="volume18.php">Volume 18 (2022)</a></p>
 		<p><a href="volume19.php">Volume 19 (2023)</a></p>
 		<p><a href="volume20.php">Volume 20 (2024)</a></p>
+		<p><a href="volume21.php">Volume 21 (2025)</a></p>
 		<p><a href="prepress.php">PrePress</a></p>
 	</div>
 </td>
---
+++ volume21.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>CANOTIA: Volume 21</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+	<link href="css/volumes.css" type="text/css" rel="stylesheet">
+</head>
+<body bgcolor="#C0C0C0" text="#000000" link="#FFFFFF" vlink="#FFFFFF" background="images/background.jpg">
+	<table border="0" cellpadding="5" style="width: 1200px; background-color: #D0F5C0; margin-left: auto; margin-right: auto;">
+		<tr>
+			<td>
+				<?php include_once('menu_header.php'); ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table cellpadding="10" align="center">
+					<tr>
+						<?php include_once('menu_left.php'); ?>
+						<td width="500" bgcolor="#FFFFFF" valign="top">
+							<div class="mid-title">Volume 21</div>
+							<div class="mid-section">
+								<div class="mid-vol" style="color: white">A Monograph of the Genus Psidium (Myrtaceae)</div>
+								<div class="mid-by">By Leslie R. Landrum</div>
+							</div>
+							<div class="mid-cover">
+								<div class="mid-vol" style="color: white">
+									<a href="volumes/vol21/Canotia-v21-cover.pdf" target="_blank"><b>Cover</b></a>. Including a Table of Contents
+								</div>
+							</div>
+							<div class="mid-section" style="color: white">
+								<div class="mid-vol">
+									<div><a href="volumes/vol21/PsidiumMonograph-part1.pdf" target="_blank"><b>Part 1</b></a>. Text including Introduction, Important Characteristics,</div>
+									<div style="margin-left: 20px">Keys, Species Treatments, Literature Cited, and</div>
+									<div style="margin-left: 20px">Tables 1–7.............................................................pages 1–211</div>
+								</div>
+							</div>
+							<div class="mid-section" style="color: white">
+								<div class="mid-vol">
+									<div><a href="volumes/vol21/PsidiumMonograph-part2.pdf" target="_blank"><b>Part 2</b></a>.
+									Species illustrations and maps..................pages 212-280</div>
+								</div>
+							</div>
+							<div style="height: 5px; background-color: #D0F5C0; margin: 20px 0px">&nbsp;</div>
+						</td>
+						<td width="10" bgcolor="#FFFFFF"></td>
+						<?php include_once('menu_right.php'); ?>
+						<td width="10" bgcolor="#FFFFFF"></td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php include_once('footer.php'); ?>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
